10b87f631fbe5e49cec575dfdde606d9111a318b
LoganRan625 <75424234+LoganRan625@users.noreply.github.com>
2021-02-01 22:21:17 -0800

    Create Ipscan.sh

--- Ipscan.sh
+++ Ipscan.sh
@@ -22,14 +22,15 @@
 #  during execution may recieve " host is down" and
 #  "host is unreachable" scroll to bottom.
 #
-#
+#---------------------------------------------------------
 #ipaddress=$(ifconfig | grep "netmask" | cut -d " " -f 6)
 #ip=$(echo $ipaddress | cut -d "." -f1-3)
+#---------------------------------------------------------
 #
 #  -----if ifconfig is working-----
 #  replace $1 with $ip and uncomment ipaddress and ip
-#  when ifconfig works, do not add ip perameter after 
-#  command "ipscan" once changes have been made
+#  when ifconfig works. when executing you will not 
+#  need to add perameter after 'ipscan' command 
 #
 #########################################################
 
@@ -41,7 +42,7 @@ for i in {1..4} ; do      # repeats incase of lost packets
   for ip in `seq 1 $2` ; do
     ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> iplist.txt &
   done
-  sleep 3 #keeps from crashing
+  sleep 3 #keeps iSH from crashing
 done
 
 iplist=$(awk '!seen[$0]++' iplist.txt) #remove duplicates
